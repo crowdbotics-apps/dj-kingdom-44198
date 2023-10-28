191006db6bbcab3e86c4b1e9c97d82934a1211a8
Crowdbotics <team@crowdbotics.com>
2023-10-28 20:12:30 +0000

    Initial commit

---
+++ backend/dj_kingdom_44198/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for dj_kingdom_44198 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "dj_kingdom_44198.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dj-kingdom-44198",
+  url: "https://dj-kingdom-44198.botics.co",
+  api: "https://dj-kingdom-44198.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
